0e1d30cf28838d9af3920c2eb5bcb8707578282e
exteriorjuice0 <0098@balcarras.gloucs.sch.uk>
2026-05-12 12:18:03 +0000

    v1.1  Form Data Sending

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello World!</h1>"
+    return render_template("signup.html")
 
+
+@app.route("/signup", methods = ["POST"])
+def signup():
+    formdata = request.form
+    username = formdata.get("username")
+    password = formdata.get("password")
+    repassword = formdata.get("repassword")
+
+    
+    print(username)
+
+    return "signing up..."
 app.run(debug=True)
 
